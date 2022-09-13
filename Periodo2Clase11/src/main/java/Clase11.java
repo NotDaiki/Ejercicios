12c3ef2a62ed62fb23fdf901d110ccab6e7eb2be
NotDaiki <113479894+NotDaiki@users.noreply.github.com>
2022-09-13 17:21:05 -0600

    Add files via upload

---
+++ Periodo2Clase11/src/main/java/Clase11.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nelsito
+ */
+public class Clase11 {
+    public static void main(String[] args) {
+    String cadena= "";
+    for (int i=1; i<=20; i++){
+    cadena = cadena + "*";
+    System.out.println(cadena);
+      }
+    }
+}
